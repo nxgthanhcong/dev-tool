e1aba8211d63d58ddeae330ae84de431ff3f1eaa
nxgthanhcong <nxgthanhcong@gmail.com>
2025-08-10 21:06:50 +0700

    add star

--- src/components/app-sidebar.tsx
+++ src/components/app-sidebar.tsx
@@ -12,6 +12,7 @@ import {
 } from "@/components/ui/sidebar";
 import { Link, NavLink } from "react-router-dom";
 import { AspectRatio } from "./ui/aspect-ratio";
+import { GitHubStars } from "./github-stars";
 
 export function AppSidebar() {
   return (
@@ -66,14 +67,6 @@ export function AppSidebar() {
       </SidebarContent>
 
       <SidebarFooter>
-        <div className="flex justify-between items-end">
-          suy sup tuoi 25
-          <img
-            src="/dev-tool/tubt.jpg"
-            alt="Photo by Drew Beamer"
-            className="h-16 w-16 rounded-lg object-cover dark:brightness-[0.2] dark:grayscale"
-          />{" "}
-        </div>
         <AspectRatio ratio={1 / 1} className="bg-muted rounded-lg">
           <img
             src="/dev-tool/qr.png"
@@ -84,7 +77,9 @@ export function AppSidebar() {
         <div className="p-4 text-sm">
           <p className="font-medium ">
             <hr />
-            badvibe4rever © 2025{" "}
+            <div>
+              badvibe4rever © 2025 <GitHubStars />
+            </div>
           </p>
         </div>
       </SidebarFooter>
